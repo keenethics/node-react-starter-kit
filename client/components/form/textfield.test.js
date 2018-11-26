c1c3f58ef44c43e17123cc978639d142698c0603
Gornostaev Sergey <svgornostaev@gmail.com>
2018-11-26 14:34:51 +0200

    Add jest and text snapshots

---
+++ client/components/form/textfield.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Textfield from './textfield';
+
+test('Render textfield without any props', () => {
+  const component = renderer.create(
+    <Textfield id="textfield-id" />,
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Render textfield with placeholder', () => {
+  const component = renderer.create(
+    <Textfield
+      id="textfield-id"
+      palceholder="Placeholder"
+    />,
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('Render disable textfield', () => {
+  const component = renderer.create(
+    <Textfield
+      id="textfield-id"
+      isDisabled
+    />,
+  );
+
+  expect(component.toJSON()).toMatchSnapshot();
+});
